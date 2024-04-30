8d9a4bccaa246d19d7014169f6bfe427c367d646
rektdie <rektdie@gmail.com>
2024-04-30 08:20:49 +0200

    added alpha beta pruning to NegaMax

--- source/search.cpp
+++ source/search.cpp
@@ -2,7 +2,7 @@
 #include "movegen.h"
 #include "evaluate.h"
 
-int NegaMax(Board board, int depth) {
+int NegaMax(Board board, int depth, int alpha, int beta) {
     if (depth == 0) return Evaluate(board);
 
     GenerateMoves(board, board.sideToMove);
@@ -12,8 +12,16 @@ int NegaMax(Board board, int depth) {
     for (Move move : board.moveList) {
         Board copy = board;
         board.MakeMove(move);
-        int score = -NegaMax(board, depth - 1);
+        int score = -NegaMax(board, depth - 1, -beta, -alpha);
         board = copy;
+
+        if (score >= beta) {
+            return beta;
+        }
+
+        if (score > alpha) {
+            alpha = score;
+        }
 
         if (score > max) {
             max = score;
@@ -33,7 +41,7 @@ Move NegaMaxHandler(Board &board, int depth) {
     for (Move move : board.moveList) {
         Board copy = board;
         board.MakeMove(move);
-        int score = -NegaMax(board, depth - 1);
+        int score = -NegaMax(board, depth - 1, -5000000, 5000000);
         board = copy;
 
         if (score > max) {
--- source/search.h
+++ source/search.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "board.h"
 
-int NegaMax(Board board, int depth);
+int NegaMax(Board board, int depth, int alpha, int beta);
 Move NegaMaxHandler(Board &board, int depth);
 void SearchPosition(Board &board, int depth);
