e7274725e67940611e9b5338a536d9c1406468b6
rektdie <rektdie@gmail.com>
2025-08-05 19:16:37 +0200

    added pawn corrhist | bench 4741642

--- source/uci.cpp
+++ source/uci.cpp
@@ -193,6 +193,7 @@ void UCILoop(Board &board) {
             ctx->killerMoves = {};
             ctx->history.Clear();
             ctx->conthist.Clear();
+            ctx->corrhist.ClearPawnHist();
             ctx->positionHistory = {};
             ctx->ss = {};
 
