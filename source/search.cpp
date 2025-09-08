a3d505aec4923b8ebc9bb7824dc52e803c902cd4
rektdie <rektdie@gmail.com>
2025-09-08 12:50:54 +0200

    changed lmp conditions | bench 6293651

--- source/search.cpp
+++ source/search.cpp
@@ -461,7 +461,7 @@ SearchResults PVS(Board& board, int depth, int alpha, int beta, int ply, SearchC
         // Late move pruning
         // If we are near a leaf node we prune moves
         // that are late in the list
-        if (!isPV && !board.InCheck() && currMove.IsQuiet() && notMated) {
+        if (currMove.IsQuiet() && notMated) {
 
             int lmpThreshold = 7 + depth * depth;
 
