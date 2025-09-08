d2586bec7de474005e2b5e627ce82e8e44960350
rektdie <rektdie@gmail.com>
2025-09-08 18:01:27 +0200

    resetting seldepth between ID iterations | bench 6293651

--- source/search.cpp
+++ source/search.cpp
@@ -723,6 +723,7 @@ static SearchResults ID(Board &board, SearchParams params, SearchContext* ctx) {
     for (int depth = 1; depth <= toDepth; depth++) {
         ctx->timeToSearch = std::max((fullTime / movesToGo) + (inc / 2), 4);
         int softTime = ctx->timeToSearch * 0.65;
+        ctx->seldepth = 0;
 
         SearchResults currentResults = PVS<true, mode>(board, depth, aw.alpha, aw.beta, 0, ctx, false);
         elapsed = ctx->sw.GetElapsedMS();
