83f7ca57fea4b48648405a94020a007d34e3ec5f
rektdie <rektdie@gmail.com>
2025-10-15 23:15:54 +0200

    depth linear margin with base | bench 5318293

--- source/search.cpp
+++ source/search.cpp
@@ -483,7 +483,7 @@ SearchResults PVS(Board& board, int depth, int alpha, int beta, int ply, SearchC
 
                 int historyScore = ctx->history[board.sideToMove][entry.bestMove.MoveFrom()][entry.bestMove.MoveTo()][sourceThreatened][targetThreatened];
 
-                if (historyScore <= -1024)
+                if (historyScore <= -2048 - 2048 * depth)
                     depth--;
             }
         }
