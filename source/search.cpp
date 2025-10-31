e5871ba5eef11b118f2134fe2b5b0efbd864a1b9
rektdie <rektdie@gmail.com>
2025-10-31 14:49:24 +0100

    changed fp lmrdepth to 7 | bench 3861397

--- source/search.cpp
+++ source/search.cpp
@@ -568,7 +568,7 @@ SearchResults PVS(Board& board, int depth, int alpha, int beta, int ply, SearchC
         int margin = fpMargin * (lmrDepth + improving) + historyScore / 32;
 
         if (!isPV && ply && currMove.IsQuiet()
-                && lmrDepth <= 5 && staticEval + margin < alpha && notMated) {
+                && lmrDepth <= 7 && staticEval + margin < alpha && notMated) {
             continue;
         }
 
