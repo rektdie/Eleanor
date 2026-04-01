f9160084a7f7b43a0c61b2869da03882659033da
rektdie <rektdie@gmail.com>
2026-04-01 20:31:36 +0200

    set tunable min/max values | bench 1398874

--- source/tunables.h
+++ source/tunables.h
@@ -2,7 +2,7 @@
 #include <algorithm>
 #include <iostream>
 
-//#define TUNING
+#define TUNING
 
 #define AUTO_STEP(min_val, max_val) ((max_val - min_val) / 20.0)
 #define AUTO_R_END_INT(min_val, max_val) \
@@ -10,45 +10,45 @@
 #define AUTO_R_END_FLOAT 0.002
 
 #define TUNABLE_LIST \
-    X_DOUBLE(lmrBaseQuiet, 1.1, 0.5, 1.5) \
-    X_DOUBLE(lmrDivisorQuiet, 2.33, 1.5, 3.5) \
-    X_DOUBLE(lmrBaseNoisy, 0.41, 0.5, 1.5) \
-    X_DOUBLE(lmrDivisorNoisy, 3.20, 1.5, 3.5) \
+    X_DOUBLE(lmrBaseQuiet, 1.1, 0.6, 1.6) \
+    X_DOUBLE(lmrDivisorQuiet, 2.33, 1.6, 3.6) \
+    X_DOUBLE(lmrBaseNoisy, 0.41, 0.1, 1.1) \
+    X_DOUBLE(lmrDivisorNoisy, 3.20, 2.0, 4.5) \
     X_INT(lmrCutnode, 2048, 1024, 3072) \
-    X_INT(lmrIsPV, 1024, 512, 2048) \
-    X_INT(lmrTTPV, 1024, 512, 2048) \
-    X_INT(lmrHistoryDivisor, 8192, 6000, 10000) \
-    X_INT(lmrImproving, 1024, 6000, 10000) \
-    X_INT(lmrCorrplexity, 1024, 6000, 10000) \
-    X_INT(lmrTTPVFailLow, 1024, 6000, 10000) \
-    X_INT(lmrDeeperBase, 30, 6000, 10000) \
-    X_INT(lmrDeeperScalar, 4, 6000, 10000) \
+    X_INT(lmrIsPV, 1024, 256, 2048) \
+    X_INT(lmrTTPV, 1024, 256, 2048) \
+    X_INT(lmrHistoryDivisor, 8192, 4096, 16384) \
+    X_INT(lmrImproving, 1024, 256, 2048) \
+    X_INT(lmrCorrplexity, 1024, 256, 2048) \
+    X_INT(lmrTTPVFailLow, 1024, 256, 2048) \
+    X_INT(lmrDeeperBase, 30, 0, 96) \
+    X_INT(lmrDeeperScalar, 4, 0, 16) \
     X_INT(rfpBase, 3200, 1600, 6400) \
     X_INT(rfpMargin, 12800, 6400, 25600) \
     X_INT(rfpImprovingMargin, 12800, 0, 25600) \
     X_INT(fpMargin, 12800, 6400, 25600) \
     X_INT(fpImprovingMargin, 12800, 0, 25600) \
     X_INT(fpHistoryMargin, 4, -64, 64) \
-    X_INT(razoringScalar, 200, 50, 200) \
-    X_INT(doubleExtMargin, 30, 10, 60) \
-    X_INT(seeQuietThreshold, -80, -150, -20) \
-    X_INT(seeNoisyThreshold, -30, -80, 20) \
-    X_INT(aspInitialDelta, 30, 20, 80) \
-    X_INT(nmpBetaMargin, 30, 20, 80) \
+    X_INT(razoringScalar, 200, 80, 320) \
+    X_INT(doubleExtMargin, 30, 8, 64) \
+    X_INT(seeQuietThreshold, -80, -160, 0) \
+    X_INT(seeNoisyThreshold, -30, -120, 40) \
+    X_INT(aspInitialDelta, 30, 8, 96) \
+    X_INT(nmpBetaMargin, 30, 8, 96) \
     X_INT(nmpBaseReduction, 768, 384, 1152) \
     X_INT(nmpImprovingReduction, 192, 0, 384) \
     X_INT(nmpDepthReduction, 64, 32, 128) \
     X_INT(nmpCaptureReduction, 192, 0, 384) \
-    X_INT(seePawnValue, 100, 80, 120) \
-    X_INT(seeKnightValue, 300, 250, 350) \
-    X_INT(seeBishopValue, 350, 300, 400) \
-    X_INT(seeRookValue, 500, 400, 600) \
-    X_INT(seeQueenValue, 900, 750, 1100) \
-    X_INT(seeQsThreshold, 0, -50, 50) \
-    X_INT(seeOrderingThreshold, -100, -120, -80) \
-    X_INT(ldseMargin, 25, -120, -80) \
-    X_INT(historyBonusMultiplier, 300, 270, 330) \
-    X_INT(historyBonusSub, 250, 230, 280)
+    X_INT(seePawnValue, 100, 80, 140) \
+    X_INT(seeKnightValue, 300, 240, 420) \
+    X_INT(seeBishopValue, 350, 250, 450) \
+    X_INT(seeRookValue, 500, 400, 700) \
+    X_INT(seeQueenValue, 900, 700, 1300) \
+    X_INT(seeQsThreshold, 0, -120, 120) \
+    X_INT(seeOrderingThreshold, -100, -200, 0) \
+    X_INT(ldseMargin, 25, 0, 96) \
+    X_INT(historyBonusMultiplier, 300, 200, 500) \
+    X_INT(historyBonusSub, 250, 128, 384)
 
 #ifdef TUNING
     #define X_DOUBLE(name, default_val, min_val, max_val) \
