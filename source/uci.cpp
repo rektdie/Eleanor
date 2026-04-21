390ee23e75eae25ca3f37c9ea32f1dc583af3882
rektdie <rektdie@gmail.com>
2026-04-22 01:23:31 +0200

    updated version number | bench 1246472

--- source/uci.cpp
+++ source/uci.cpp
@@ -301,7 +301,7 @@ static void SetOption(std::string& command, SEARCH::SearchContext* ctx) {
 }
 
 static void PrintEngineInfo() {
-    std::cout << "id name Eleanor v4.0" << std::endl;
+    std::cout << "id name Eleanor v4.1" << std::endl;
     std::cout << "id author rektdie" << std::endl;
     std::cout << "option name Hash type spin default 8 min 1 max 1024" << std::endl;
     std::cout << "option name Threads type spin default 1 min 1 max 512" << std::endl;
